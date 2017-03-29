8ccab2fc2450691600cc32c84f84ffd066e56bb6
KalyaniT <kalyani@enabledware.com>
2017-03-30 00:41:04 +0530

    Testimonials Module

---
+++ app/code/community/TM/Core/sql/tm_core_setup/mysql4-install-1.0.0.php
@@ -0,0 +1,29 @@
+<?php
+
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+if ($installer->getConnection()->isTableExists($installer->getTable('tmcore/module')) != true) {
+    /**
+     * Create table 'tmcore/module'
+     */
+    $typeText = defined('Varien_Db_Ddl_Table::TYPE_TEXT') ? Varien_Db_Ddl_Table::TYPE_TEXT : Varien_Db_Ddl_Table::TYPE_VARCHAR;
+
+    $table = $installer->getConnection()
+            ->newTable($installer->getTable('tmcore/module'))
+            ->addColumn('code', $typeText, 50, array(
+                'nullable' => false,
+                'primary' => true,
+                    )
+            )
+//    ->addColumn('version', Varien_Db_Ddl_Table::TYPE_TEXT, 50)
+            ->addColumn('data_version', $typeText, 50)
+            ->addColumn('license_key', $typeText, 32);
+    $installer->getConnection()->createTable($table);
+}
+
+
+$installer->endSetup();
---
+++ app/code/local/TM/Testimonials/Block/List/Customtestimonials.php
@@ -0,0 +1,6 @@
+<?php
+
+class TM_Testimonials_Block_List_Customtestimonials extends Mage_Core_Block_Template
+{
+    
+}
